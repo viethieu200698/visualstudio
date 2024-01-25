dfb9c56ed012167809188351e37fa44ef320964f
nguyen_viet_hieu <viethieu200698@gmail.com>
2024-01-25 18:30:54 +0700

    var

--- js/demo.js
+++ js/demo.js
@@ -1,2 +1,23 @@
-// đây là ngôn ngữ js
-console
+// đây là ngôn ngữ logic
+var x; //x không có kiểu dữ liệu = null
+x = 10;// x: number
+var y = "hello world";// string
+var t = true //boolean
+
+//khai báo biến kiểu mới;
+let n = 20;
+
+var z = x + 10; //20
+var z1 = x + y; //"10hello world"
+var z2 = y + y; // "hello worldhello world"
+var z3 = "20" + 10; //"2010"
+console.log(x+y+"20"+10);// "10hello world2010"
+
+if(x>=10){
+    
+}else{
+
+}
+for(var i=0;i<10;i++){
+    console.log("i="+i);
+}
