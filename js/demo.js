fed86febce94c3e9c1942a9b8280288716e3553e
nguyen_viet_hieu <viethieu200698@gmail.com>
2024-01-26 13:33:58 +0700

    jssfull

--- js/demo.js
+++ js/demo.js
@@ -1,36 +1,37 @@
 // đây là ngôn ngữ logic
-var x; //x không có kiểu dữ liệu = null
-x = 10;// x: number
-var y = "hello world";// string
-var t = true //boolean
+var x; // x ko có kiểu dữ liệu  = null
+x  = 10;// x : Number
+var y = "hello world!"; // String
+var t = true; // boolean
 
-//khai báo biến kiểu mới;
+// khai báo biến kiểu mới
 let n = 20;
 
-var z = x + 10; //20
-
-var z1 = x + y; //"10hello world"
-var z2 = y + y; // "hello worldhello world"
-var z3 = "20" + 10; //"2010"
+var z = x + 10; // 20
+z = "xin chao";
+var z1 = x + y; // "10hello world"
+var z2 = y+y; // "hello worldhello world"
+var z3 = "20" + 10;// "2010"
 console.log(x+y+"20"+10);// "10hello world2010"
 
 
 if(x>=10){
-    
+
 }else{
 
 }
+
 for(var i=0;i<10;i++){
     console.log("i="+i);
 }
 
 var arr = [];
-arr[0]=2;
-arr[1]="abc";
+arr[0] = 2;
+arr[1] = "abc";
 
-arr.push(135); //arr[2]=135;
-arr.push("hello"); // arr[3]="hello";
-console.log("size of arr:"+arr.length);
+arr.push(135); // arr[2] = 135;
+arr.push("hello"); // arr[3] = "hello"
+console.log("Size of arr: "+arr.length);
 
 for(var i=0;i<arr.length;i++){
     console.log(arr[i]);
@@ -41,53 +42,75 @@ arr.forEach(e=>{
 })
 
 function tinh_tong(a,b){
-    //thích làm gì thì làm
-    //return a+b;
+    // thích làm gì thì làm
+    // return a+b;
     console.log(a+b);
-
 }
 tinh_tong(5,7);
 tinh_tong(5,"ABC");
 
-//alert("yêu cầu phải trên 18 tuổi"); //void
-var tl = confirm("bạn đã chắc chắn chưa?"); //boolean
-if(t){
-    alert("người dùng đã chắc chắn với câu trả lời");
+// alert("Yêu cầu phải trên 18 tuổi"); // void
 
-}else{
-    alert("người dùng cần tìm hiểu thêm");
+// var tl = confirm("Bạn chắc chắn chưa?"); // boolean
+// if(tl){
+//     alert("Người dùng đã chắc chắn với câu trả lời");
+// }else{
+//     alert("Người dùng cần tìm hiểu thêm");
+// }
 
-}
-prompt("bạn bao nhiêu tuổi"); //string "18"
-//var kq2 = parseInt(kq) + 10; //1810
-
-alert("bạn đã "+kq("tuổi"));
+// var kq = prompt("Bạn bao nhiêu tuổi");// string "18"
+// var kq2 = parseInt(kq) +10; // 1810
+/*
+    "18" => 18
+    "18a" => 18
+    "18a2"=> 18
+    "a18"=> NaN ( not a number)
+*/
+// alert("Sau 10 năm, bạn đã "+kq2+" tuổi");
 
 var rd = Math.random(); // 0<rd<1 0.00291291923
-rd = rd * 100; //0.01244 -> 1.244
-rd = parseInt(rd);
+rd = rd * 100; // 0.01244 -> 1.244
+rd = parseInt(rd); 
+console.log(rd);
+// Cho người dùng nhập vào 1 số bất kỳ (0->99)
+// sinh ra số ngẫu nhiên và xem họ có trúng giải hay không
+// var dd = prompt("Nhập 1 giá trị: ");
+// dd = parseInt(dd);
+// if(dd == rd){
+//     alert("Chúc mừng bạn đã trúng 1 tỷ");
+// }else{
+//     alert("Hẹn gặp lại bạn vào ngày mai hoặc ở đê");
+// }
 
-var randomNumber= Math.floor(Math.random()*100);
-var userNumber = prompt("nhap từ 0 đến 99:");
-if(parseInt(userNumber)===randomNumber){
-alert("chúc mừng! bạn đã trúng");
-}else{
-alert("rất tiếc! số đúng là"+randomNumber);
-}
+// tạo ra 1 bộ 23 số (0->99)
+// người chơi sẽ chọn 1 số mỗi lần ( tối đa 7 lần chơi) và số tiền đặt 
+// cược của ngày hôm đó. (giả sử tỉ lệ là 23đ 1 điểm -> trúng 1 số thì đc 80đ)
+// Sau 7 ngày hãy tính số tiền đã bỏ và số tiền thu được
 
-var numberSet = [];
-for(var i = 0;i < 23;i++){
-    numberSet.push(math.foor(Math.random()*100));
+var st = 0;
+var dt = 0;
+for(var i=1;i<=7;i++){
+    var kq = [];
+    for(var j=0;j<23;j++){
+        var rd = Math.random() * 100;
+        rd = parseInt(rd);
+        kq.push(rd);
+    }
+    var lc = prompt(`Nhập số muốn đặt hôm nay (ngày thứ ${i}):`);
+    lc = parseInt(lc);
+    var td = prompt(`Nhập số tiền muốn đặt của hôm nay(ngày thứ ${i}):`);
+    td = parseInt(td);
+    st+= td;
+    kq.forEach(e=>{
+        if(e ==lc){
+            dt+= (td/23)*80;
+        }
+    })
 }
-var tong_diem = 0;
-for( luot_choi = 1;  luot_choi <=7 ;luot_choi++){
-var userNumber = prompt (+luot_choi+);
-var cuoc_tien = prompt;
-if(numberSet.incudes(parseInt(userNumber))){
-    tong_diem += 80;
-    alert("chuc mung ban nhan duoc 80 diem.");
+alert(`Tổng tiền ban đầu: ${st} và số tiến thu được ${dt}`);
 
-} else{
-    alert("rất tiếc số đúng là "+ numberSet.point +)
-}
-}
+var t = 20;
+
+var str = "Năm nay tôi 20 tuổi";
+var str2 = 'Năm nay tôi '+t+' tuổi';
+var str3 = `Năm nay tôi ${t} tuổi`;
