46562287880556479f00318711d72141809aad93
nguyen_viet_hieu <viethieu200698@gmail.com>
2024-01-26 20:53:38 +0700

    s

--- demo2_js.php
+++ demo2_js.php
@@ -3,19 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>demojs2</title>
+    <title>Layout</title>
+    <!-- Font google -->
     <?php include("html/css.html");?>
-
 </head>
 <body>
     <main class="section main">
         <div class="container">
-            <h1>demo js2</h1>
+            <h1>Demo JS2</h1>
             <h2 id="number">10</h2>
-            <h2><span>10</span><span>00</span></h2>
+            <h2>
+                <span id="min">10</span>
+                :
+                <span id="sec">00</span>
+            </h2>
         </div>
     </main>
-    <script type="text/javascript" src="js/demo2.js"></script>
-    
+   <script type="text/javascript" src="js/demo2.js"></script>
 </body>
 </html>
--- js/demo2.js
+++ js/demo2.js
@@ -1,34 +1,40 @@
-/*setTimeout(function(){
-    alert("Hello everybody");
-},2000);
+// setTimeout(function(){
+//     alert("Hello everybody");
+// },2000);// delay 2000 ms
 
-setInterval(function(){
-    alert("Hello T2311E");
-},2000);*/
 var h = document.getElementById("number");
+// h.innerText = "XIN CHAO CAC BAN";
+
 var t=10;
 var it = setInterval(function(){
-
-   // console.log(t);
-   h.innerText = t;
+    // alert("Hello T2311E");
+    // console.log(t);
+    h.innerText = t;
     t--;
     if(t<0){
         clearInterval(it);
     }
 },1000);
-/*var min = 10, sec = 0;
 
-  var timer = setInterval(function(){
-    console.log(`${min}l:${sec}`);
+// 10:00
+// 09:59
+// 09:58
+//....
+// 00:00
+
+var span_min = document.getElementById("min");
+var span_sec = document.getElementById("sec");
+var min = 10, sec=0;
+var timer = setInterval(function(){
+    // console.log(`${min}:${sec}`);
+    span_min.innerText = min;
+    span_sec.innerText = sec;
     sec--;
     if(sec<0){
         min--;
         sec = 59;
-
     }
     if(min<0){
         clearInterval(timer);
     }
-  }
-  ,10);*/
-
+},100);
